a6b8626de5fb2eb045499fba53fcfa81fc36226a
Juan Pardo <josefrecinosanitarios@yahoo.com.ar>
2023-03-12 17:45:27 -0300

    add font family

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       className='flex items-center justify-center h-screen px-5 '
       style={{ backgroundColor: 'hsl(212, 45%, 89%)' }}
     >
-      <div className='max-w-md w-[300px]   bg-white rounded-2xl  shadow-lg p-4'>
+      <div className='max-w-md w-[300px] h-[500px]   bg-white rounded-2xl  shadow-lg p-4'>
         <div className='h-64 overflow-hidden'>
           <img
             src={qrImg}
@@ -14,8 +14,8 @@ function App() {
             className='object-cover object-center h-full w-full rounded-lg'
           />
         </div>
-        <div className='p-4'>
-          <h2 className='text-xl font-bold text-gray-800 mb-5 text-center'>
+        <div className=' '>
+          <h2 className='text-xl font-bold text-gray-800 mb-5 text-center mt-5'>
             Improve your front-end skills by building projects
           </h2>
           <p className='mt-2 text-gray-400 font-[15px] text-center'>
