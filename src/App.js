6035f75c82337dbe8b7636625adf047f3b109431
Juan Pardo <josefrecinosanitarios@yahoo.com.ar>
2023-03-12 17:36:07 -0300

    add qr card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import qrImg from './assets/image-qr-code.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className='flex items-center justify-center h-screen px-5 '
+      style={{ backgroundColor: 'hsl(212, 45%, 89%)' }}
+    >
+      <div className='max-w-md w-[300px]   bg-white rounded-2xl  shadow-lg p-4'>
+        <div className='h-64 overflow-hidden'>
+          <img
+            src={qrImg}
+            alt='Placeholder'
+            className='object-cover object-center h-full w-full rounded-lg'
+          />
+        </div>
+        <div className='p-4'>
+          <h2 className='text-xl font-bold text-gray-800 mb-5'>
+            Improve your front-end skills by building projects
+          </h2>
+          <p className='mt-2 text-gray-400 font-[15px] text-center'>
+            Scan the QR code to visit! Frontend Mentor and take your coding
+            skills to the next level!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
